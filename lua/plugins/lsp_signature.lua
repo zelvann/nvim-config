18333a38ace7e73c6657290790ecc38eb55e036a
Zelvan Abdi Wijaya <zelvan1701@gmail.com>
2025-01-27 21:49:07 +0700

    feat: add lsp signature

---
+++ lua/plugins/lsp_signature.lua
@@ -0,0 +1,49 @@
+return {
+	"Dan7h3x/signup.nvim",
+	branch = "main",
+	enabled = true,
+	opts = {
+		silent = false,
+		number = true,
+		icons = {
+			parameter = "",
+			method = "󰡱",
+			documentation = "󱪙",
+		},
+		colors = {
+			parameter = "#86e1fc",
+			method = "#c099ff",
+			documentation = "#4fd6be",
+			default_value = "#a80888",
+		},
+		active_parameter_colors = {
+			bg = "#86e1fc",
+			fg = "#1a1a1a",
+		},
+		border = "solid",
+		winblend = 10,
+		auto_close = true,
+		trigger_chars = { "(", "," },
+		max_height = 10,
+		max_width = 40,
+		floating_window_above_cur_line = true,
+		preview_parameters = true,
+		debounce_time = 30,
+		dock_toggle_key = "<Leader>sd",
+		toggle_key = "<C-k>",
+		dock_mode = {
+			enabled = false,
+			position = "bottom",
+			height = 3,
+			padding = 1,
+		},
+		render_style = {
+			separator = true,
+			compact = true,
+			align_icons = true,
+		},
+	},
+	config = function(_, opts)
+		require("signup").setup(opts)
+	end,
+}
